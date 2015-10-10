a208835f5e8ac31acd29d9cfc0bedd85b63dd1ef
Victor <i.am.victor.fong@gmail.com>
2015-10-10 18:31:08 -0400

    Add basic usage of struct and interface

--- src/gods/god.go
+++ src/gods/god.go
@@ -5,22 +5,38 @@ package gods
 const (
 	BUY = 1 << iota  // a == 1 (iota has been reset)
     SELL = 1 << iota  // b == 2
-	DO_NOTHING = 1 << iota  // c == 4
 )
-//
-//type Choice struct {
-//	action uint8
-//	amount uint16
-//}
 
-//type god interface {
-//	decide(openings []OpenMarket) Choice
-//	name() string  
-//}
-//
-//type Fool interface {
-//	
-//}
+type Choice struct {
+	symbol string
+	action int
+	amount int
+}
+
+type Track struct {
+	symbol string
+	open float32
+	high float32
+	low float32
+	end float32
+}
+
+type God interface {
+	decide(openings []Track) []Choice
+	name() string  
+}
+
+type Fool struct {
+	
+}
+
+func (fool Fool) name() string{
+	return "Fool ~_~"
+}
+
+func (fool Fool) decide(openings []Track) []Choice{
+	return []Choice{}
+}
 
 //func (fool Fool) decide(openings []OpenMarket) Choice{
 //	action := rand.Int()
--- src/gods/god_test.go
+++ src/gods/god_test.go
@@ -13,9 +13,62 @@ func TestStub(t *testing.T) {
 func TestActions(t *testing.T) {
 	fmt.Printf("Testing")
 	assert.NotEqual(t, BUY, SELL)
-	assert.NotEqual(t, SELL, DO_NOTHING)
 }
 
-//func TestChoice(){
-//	Choice choice = Choice{action: 
-//}
+func TestChoice(t *testing.T) {
+	symbol := "abcd"
+	action := BUY
+	amount := 5000
+	
+	choice1 := Choice{symbol: symbol, action: action, amount: amount}
+	assert.Equal(t, choice1.symbol, symbol)
+	assert.Equal(t, choice1.action, action)
+	assert.Equal(t, choice1.amount, amount)
+}
+
+func TestTrack(t *testing.T){
+	symbol := "abcd"
+	var open float32 = 15.23
+	var low float32 = 14.39
+	var high float32 = 15.85
+	var end float32 = 15.80
+	
+	track1 := Track{
+		symbol: symbol,
+		open: open,
+		low: low,
+		high: high,
+		end: end,
+	}
+	
+	assert.Equal(t, track1.symbol, symbol)
+	assert.Equal(t, track1.open, open)
+	assert.Equal(t, track1.low, low)
+	assert.Equal(t, track1.high, high)
+	assert.Equal(t, track1.end, end) 
+}
+
+func TestFoolName(t *testing.T){
+	fool := Fool{}
+	assert.Equal(t, fool.name(), "Fool ~_~")
+}
+
+func TestFoolDecide(t *testing.T){
+	fool := Fool{}
+	empty_array := []Choice{}
+	result := fool.decide(nil)
+	assert.Equal(t, result, empty_array)
+	assert.Equal(t, len(result), 0)
+	
+	track1 := Track{symbol: "abcd", open: 15.01}
+	tracks := []Track {track1}
+	result = fool.decide(tracks)
+	assert.Equal(t, result, empty_array)
+	assert.Equal(t, len(result), 0)
+	
+	track2 := Track{symbol: "edads", open: 15.01}
+	tracks = []Track {track1, track2}
+	result = fool.decide(tracks)
+	assert.Equal(t, result, empty_array)
+	assert.Equal(t, len(result), 0)
+}

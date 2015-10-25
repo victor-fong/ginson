2ce5d54c48a5620dae8bae9ce8f552a10f4e1447
Victor Fong <i.am.victor.fong@gmail.com>
2015-10-25 15:11:22 -0400

    Add Portfolio

--- src/gods/arena.go
+++ src/gods/arena.go
@@ -4,7 +4,47 @@ import (
 
 )
 
-//func battle(gods []God, dataProviders []DataProvider){
-//	
-//}
+type Long struct {
+	symbol string
+	entry float64
+	amount int64
+}
+
+type Short struct {
+	symbol string
+	entry float64
+	amount int64
+}
+
+type Portfolio struct {
+	Cash float64
+	Total float64
+	Longs []Long
+	Shorts []Short
+}
+
+const(
+	InitCash float64 = 10000.0
+)
+
+func InitPortfolio() Portfolio{
+	var result Portfolio = Portfolio{
+		Cash: InitCash,
+		Total: InitCash,
+		Longs: make([]Long, 0),
+		Shorts: make([]Short, 0),
+	}
+	
+	return result
+}
+
+func battle(gods []God, dataProviders []DataProvider){
+	var portfolios map[God]Portfolio = make(map[God]Portfolio)
+	for _, god := range gods {
+		portfolios[god] = InitPortfolio()
+	}
+	
+	
+	
+}
 
--- src/gods/arena_test.go
+++ src/gods/arena_test.go
@@ -1,6 +1,15 @@
 package gods
 
 import (
-
+	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
+func TestInitPortfolio(t *testing.T) {
+	var portfolio Portfolio = InitPortfolio()
+	
+	assert.Equal(t, InitCash, portfolio.Cash)
+	assert.Equal(t, InitCash, portfolio.Total)
+	assert.Equal(t, 0, len(portfolio.Longs))
+	assert.Equal(t, 0, len(portfolio.Shorts))
+}
